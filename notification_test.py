ffb5c50b2d6eadb0c562d490d91c1312221a7661
ortizbar <ortizbar@oregonstate.edu>
2026-05-20 19:40:20 -0600

    Add test for sending notification via POST request

--- notification_test.py
+++ notification_test.py
@@ -1,1 +1,12 @@
+import requests
+url = "http://localhost:5001/send-notification"
 
+data = {
+  'user': "test123",
+  'notification_type':"Error",
+  'message': "Image generation failed" }
+
+response = requests.post(url, json=data)
+
+print("Status code:", response.status_code)
+print("Response JSON:", response.json())
