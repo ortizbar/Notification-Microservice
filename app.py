64a79b4b9ae6e91281ce462610eec4af0659abbe
ortizbar <ortizbar@oregonstate.edu>
2026-05-20 19:31:44 -0600

    Add send_notification endpoint with validation
    Implement a Flask endpoint to send notifications with validation for required data and notification types.

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
+from flask import Flask, request, jsonify
+app = Flask(_name_)
 
+@approute('/send-notification', methods=['POST'])
+def send_notification ():
+  data = request.get_json()
+
+#checking for missing data
+  if (
+    "user" not in data or
+    "notification" not in data or
+    "message" not in data ):
+      return jsonify({'status': "Error", 'message': "Required data not found"}), 400
+
+  user = data["user"]
+  notification = data["notification_type"]
+  message = data["message"]
+
+#notification types validation
+  valid_types = ["error","status","success"]
+  
+  if notification_type not in valid_types:
+    return jsonify({'status': "Error", 'message': "Invalid notification type"}), 400
+  return jsonify ({
+    'status': "sent",
+    'user': user,
+    'notification_type': notification_type,
+    'message': message,
+    'delivery_status': "Notification delivered successfully" }), 200
+
+if _name_ == '_main_':
+  app.run(host= "0.0.0.0" , port = 5001)
+  
+
+
+    
